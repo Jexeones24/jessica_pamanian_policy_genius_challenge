ef692a70094708b4cc18fd4f95b51fabdee611a3
Jexeones24 <jexeones@gmail.com>
2017-10-23 10:54:11 -0400

    removed server

--- index.js
+++ index.js
@@ -1,46 +1,77 @@
-const http = require('http')
-const fs = require('fs')
-const formidable = require('formidable')
-const util = require('util')
-const User = require('./models/user.js')
-
-const server = http.createServer(function (req, res) {
-  if (req.method.toLowerCase() === 'get') {
-    displayFormData(res)
-  } else if (req.method.toLowerCase() === 'post') {
-    processFields(req, res)
+class Condition {
+  constructor (name) {
+    this.name = name
+    this.getFactor = () => {
+      if (name === 'allergies') {
+        return 0.01
+      } else if (name === 'sleep apnea') {
+        return 0.06
+      } else {
+        return 0.17
+      }
+    }
   }
+}
+
+class User {
+  constructor (name, age, gender, condition, baseCost) {
+    this.name = name
+    this.age = age
+    this.gender = gender
+    this.condition = condition
+    this.baseCost = 100
+  }
+  yearsOver18 () {
+    let min = 18
+    let totalYrs = this.age - min
+    return this.age >= min ? Math.floor(totalYrs / 5) : 'Life insurance is not available for minors'
+  }
+  adjustBaseCostForAge () {
+    let num = this.yearsOver18()
+    let extra = num * 20
+    return this.baseCost + extra
+  }
+  conditionFactor () {
+    let condition = new Condition(this.condition)
+    let factor = condition.getFactor()
+    let adjustedBaseCost = this.adjustBaseCostForAge()
+    let extra = adjustedBaseCost * factor
+    return adjustedBaseCost + extra
+  }
+  discount () {
+    let total = this.conditionFactor()
+    return this.gender === 'female' ? (total - 12).toFixed(2) : total.toFixed(2)
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('userForm').addEventListener('submit', makeQuote)
 })
 
-const displayFormData = (res) => {
-  fs.readFile('index.html', (err, data) => {
-    res.writeHead(200, {
-      'Content-Type': 'text/html',
-      'Content-Length': data.length
-    })
-    res.write(data)
-    res.end()
-  })
+function makeQuote (event) {
+  event.preventDefault()
+  let form = document.getElementById('userForm')
+  let name = document.getElementById('name').value
+  console.log(name)
+  let age = document.getElementById('age').value
+  console.log(age)
+  let gender = document.querySelector('input[name="gender"]:checked').value
+  console.log(gender)
+  let condition = document.getElementById('condition').value
+  console.log(condition)
+  let user = new User(name, age, gender, condition)
+  console.log(user)
+  let quote = user.discount()
+  form.reset()
+  displayQuote(user, quote)
 }
 
-const processFields = (req, res) => {
-  let form = new formidable.IncomingForm()
-  form.parse(req, (err, fields, files) => {
-    res.writeHead(200, {
-      'content-type': 'application/json'
-    })
-    res.write('how in the world...')
-    res.end(JSON.stringify(fields), () => {
-      let name = fields.name
-      let age = Number(fields.age)
-      let gender = fields.gender
-      let condition = fields.condition
-      let user = new User(name, age, gender, condition)
-      let quote = user.discount()
-      console.log('quote:', quote)
-    })
-  })
+function displayQuote (user, quote) {
+  let target = document.getElementById('quote')
+  let h3 = document.createElement('h3')
+  h3.innerHTML = 'Your Insurance Quote:'
+  let p = document.createElement('p')
+  p.innerHTML = user.name + ' - ' + '$' + quote
+  target.parentNode.insertBefore(h3, target)
+  target.parentNode.insertBefore(p, target)
 }
-
-server.listen(3000)
-console.log('listening on port 3000')
